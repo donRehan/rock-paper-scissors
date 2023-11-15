897da9561ef58f03361a889165f20b13b135491b
DonRehan <30264386+donRehan@users.noreply.github.com>
2023-11-15 22:26:59 +0200

    Add loop incase of wrong input
    Add a loop that goes on until the user provides correct input format
    else re ask the user for input.
    
    [Practice Commit messages]

--- main.js
+++ main.js
@@ -5,17 +5,25 @@ let getComputerChoice = () => {
 }
 
 let getUserChoice = () => {
-	console.log("Please select one from the following: ");
-	console.log("0. rock");
-	console.log("1. paper");
-	console.log("2. scissor");
-	let choice = parseInt(prompt("Enter your choice: "));
-	// to be removed
-	//console.log(choice);
-	if(!validateInput(choice)) {
-		console.log("Invalid Choice please select a number from 0 to 2");
-		return;
+	let flag = true;
+	let choice = null;
+
+	while (flag)
+	{
+		flag = false;
+		console.log("Please select one from the following: ");
+		console.log("0. rock");
+		console.log("1. paper");
+		console.log("2. scissor");
+		choice = parseInt(prompt("Enter your choice: "));
+		// to be removed
+		//console.log(choice);
+		if(!validateInput(choice)) {
+			console.log("Invalid Choice please select a number from 0 to 2");
+			flag = true;
+		}
 	}
+
 	let choices = ['rock', 'paper', 'scissor'];
 	let selection = choices[choice];
 	return selection;
@@ -24,4 +32,4 @@ let getUserChoice = () => {
 let validateInput = (_) => (_ >= 0 && _ <= 2) ? true : false;
 
 // to be removed
-console.log(getUserChoice());
+//console.log(getUserChoice());
